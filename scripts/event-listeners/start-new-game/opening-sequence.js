2848011ceab90c9243f372cbd8ae7ad33b12ba30
Colin <cstatro@gmail.com>
2019-08-20 10:39:05 -0400

    added top text

--- scripts/event-listeners/start-new-game/opening-sequence.js
+++ scripts/event-listeners/start-new-game/opening-sequence.js
@@ -1,6 +1,8 @@
+import { greetingText1 } from "../../templates/start-new-game/01_Text.js";
+
 export const startOpeningSequence = json => {
   const gameArea = document.querySelector(".game-area");
-  gameArea.innerHTML = "";
+  gameArea.innerHTML = greetingText1();
   console.log(json);
-  gameArea.dataset.user = json;
+  gameArea.dataset.user = json.name;
 };
