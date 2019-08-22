be14c36c058afd05e4c47190b89a3c2c3a2b156f
Colin <cstatro@gmail.com>
2019-08-21 23:36:53 -0400

    added background

--- scripts/templates/title-card.js
+++ scripts/templates/title-card.js
@@ -1,4 +1,5 @@
-export const titleScreen = () => `<div class="title-area">
+export const titleScreen = () => `
+<div class="title-area">
   <h1 class="title-text">PokeFallout</h1>
-  <h2 class="start-button">Start Game</h2>
+  <h2 class="start-button">Start Game</h2> 
 </div>`;
