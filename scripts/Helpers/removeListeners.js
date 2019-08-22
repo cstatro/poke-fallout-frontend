8a8662158454f001b98468ca7785e7cbbbb24b05
Colin <cstatro@gmail.com>
2019-08-22 15:11:19 -0400

    all event listener / dom removal has been implemented

--- scripts/Helpers/removeListeners.js
+++ scripts/Helpers/removeListeners.js
@@ -1,11 +1,21 @@
 export const removeEventListeners = () => {
   removeNavListeners();
+  removeConfirmListeners();
+  removePokemonDivs();
 };
 
 const removeNavListeners = () => {
-  document.querySelector("#nav-bar").removeEventListener("click", e => {
-    if (e.target.id != "clean") {
-      highLightButton(e.target.closest("button"));
-    }
-  });
+  const nav = document.querySelector("#nav-bar");
+  const cloneNav = nav.cloneNode(true);
+  nav.parentNode.replaceChild(cloneNav, nav);
+};
+
+const removeConfirmListeners = () => {
+  const confirm = document.getElementById("confirm");
+  const cloneConfirm = confirm.cloneNode(true);
+  confirm.parentNode.replaceChild(cloneConfirm, confirm);
+};
+
+const removePokemonDivs = () => {
+  document.querySelector("#pokemon-display").innerHTML = "";
 };
