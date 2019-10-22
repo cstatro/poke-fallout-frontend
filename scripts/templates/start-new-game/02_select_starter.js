d3c20f1b31650dd568a997abcf84b4e1a001a88b
Colin <cstatro@gmail.com>
2019-10-22 15:28:53 -0400

    fixed join issue

--- scripts/templates/start-new-game/02_select_starter.js
+++ scripts/templates/start-new-game/02_select_starter.js
@@ -1,13 +1,11 @@
 const dispStarter = obj => `<div data-id=${obj.id} class="starter-box">
 <h2 class="starter-name">${obj.attributes.name}</h2>
 <img src="${obj.attributes.image_url}" alt="" />
-<div class="starter-details"><p>gender:${obj.attributes.gender}</p><p>HP:${
-  obj.attributes.hp
-}</p></div>
+<div class="starter-details"><p>gender:${obj.attributes.gender}</p><p>HP:${obj.attributes.hp}</p></div>
 </div>
 `;
 
-export const dispStarters = arr => arr.map(p => dispStarter(p)).join();
+export const dispStarters = arr => arr.map(p => dispStarter(p)).join("");
 
 export const starterDiv = () => `<div class="starter-div"></div>`;
 
